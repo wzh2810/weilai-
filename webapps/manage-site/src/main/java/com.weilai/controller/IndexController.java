98d0c7ecc384ba3720be0f8aa652a36dd6a85b6d
wangzhen\wang <wangzhen@shweilai.cn>
2019-08-29 23:45:48 +0800

    add

--- webapps/manage-site/src/main/java/com.weilai/controller/IndexController.java
+++ webapps/manage-site/src/main/java/com.weilai/controller/IndexController.java
@@ -1,9 +1,11 @@
 package com.weilai.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("")
+@Controller
+@RequestMapping("wl/")
 public class IndexController {
 
     @RequestMapping("index")
